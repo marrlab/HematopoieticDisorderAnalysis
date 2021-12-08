94c50c4306b4301777a097e7e4d221d2f804ac37
Lisa Bast <lisa.bast@ki.se>
2021-12-08 17:07:33 +0100

    order of individuals in results mat
    changed for paper figures

--- MATLAB/parameter_inference/save_par_values_main.m
+++ MATLAB/parameter_inference/save_par_values_main.m
@@ -4,13 +4,31 @@ clc;
 
 [opt] = setPaths();
 fileName = '2020_08_data.xlsx';
-group_str = 'MDS';% 'healthy';%
-bool_fit_repetitions_seperately=false;
-individual_IDs = sort(getIndividuals(fileName,group_str,bool_fit_repetitions_seperately));
-%healthy: '311'    '312'    '345'    '348'    '353'    '370'    '380'    '391'    '439'    '482'    '500'    '508'    '519'    '520'    '522'    '547'    '552'    '559'    '560'    '561'    '607'    '657'    '775'    '791'
-%MDS: '135'    '140'    '227'    '279'    '326'    '354'    '360'    '373'    '377'    '620'
-% individual_IDs = {'353_1','345_1','559_1','560_1','482_1','522_1'};
-% individual_IDs = {'1'};%,'2','3'};
+group_str =  'healthy';%'MDS';%
+bool_fit_repetitions_seperately=false;%true;%
+
+if strcmp(group_str,'MDS')
+    %individuals sorted by risk factor
+    if bool_fit_repetitions_seperately==true
+        %individual_IDs = {'227_1','373_1','326_1','360_1','135_1','354_1','140_1','279_1','377_1','620_1'};
+        individual_IDs = {'373_1','326_1','360_1','227_1','135_1','354_1','279_1','377_1','140_1','620_1'};
+    else
+        %individual_IDs = {'227','373','326','360','135','354','140','279','377','620'};
+        individual_IDs = {'373','326','360','227','135','354','279','377','140','620'};
+    end
+else
+    if bool_fit_repetitions_seperately==true
+        individual_IDs = {'353_1','657_1','311_1','380_1','791_1','607_1','312_1','312_2','559_1','345_1','391_1','391_2','560_1', '561_1', '348_1', '552_1', '775_1'};   
+    else
+        individual_IDs = {'353','657','311','380','791','607','312','559','345','391','560', '561', '348', '552', '775'};  
+    end
+    %individual_IDs = sort(getIndividuals(fileName,group_str,bool_fit_repetitions_seperately));
+   
+    %healthy: '311'    '312'    '345'    '348'    '353'    '370'    '380'    '391'    '439'    '482'    '500'    '508'    '519'    '520'    '522'    '547'    '552'    '559'    '560'    '561'    '607'    '657'    '775'    '791'
+    %MDS: '135'    '140'    '227'    '279'    '326'    '354'    '360'    '373'    '377'    '620'
+    % individual_IDs = {'353_1','345_1','559_1','560_1','482_1','522_1'};
+    % individual_IDs = {'1'};%,'2','3'};
+end
 
 opt_RUN = 'fit_samples';%'test_inference_procedure';%%'hierarchy_comparison_BIC';%
 
